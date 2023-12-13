2ab2e96b7cac26bc720151b333fddbf3704b6097
AnasZbeedy <anaszbeedy1996@gmail.com>
2023-12-13 16:59:04 +0300

    Wallet

---
+++ Expense_Track/Expense Track/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Track.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) 
+        { 
+
+        }
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<Category> categories { get; set; }
+    }
+}
---
+++ Expense_Track/Expense Track/Models/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Track.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Range(1,int.MaxValue,ErrorMessage ="Please Select a Category")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Amount Should Be Greater Than 0")]
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+        public DateTime Data { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string CategoryTitlewithicon 
+        {
+            get
+            {
+                return Category==null ? "" : Category.Icon+" "+Category.Title;
+            }
+        }
+        [NotMapped]
+        public string FormatAmount
+        {
+            get
+            {
+                return ((Category==null || Category.Type=="Expense") ? " - " : " + " )+ Amount.ToString("C0");
+            }
+        }
+    }
+
+}
